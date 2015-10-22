ac51a963186b82f1b0452081cd14985c2b075aa5
Nina Stavnsbjerg <s144477@win.dtu.dk>
2015-10-22 13:33:46 -0700

    First try

---
+++ AlgoTreeDraw/AlgoTreeDraw/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AlgoTreeDraw
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void RDChecked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = null;
+            rb = e.OriginalSource as RadioButton;
+            if (String.Equals(rb.Content, "Nina"))
+            {
+                MessageBox.Show("You are absolutely right!");
+            }
+            else
+            {
+                Random random = new Random();
+                int n = random.Next(0, 3);
+                String[] answers = { "No!", "Wrong!", "Not Likely" };
+                MessageBox.Show(answers[n]);
+            }
+        }
+    }
+}
