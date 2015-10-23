40617b0396debd1f41d739d86a65b4a04e93cb66
Alemil <s134073@student.dtu.dk>
2015-10-23 15:36:35 +0200

    Alle klasser + nodevisning

---
+++ AlgoTreeDraw/AlgoTreeDraw/Model/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace AlgoTreeDraw.Model
+{
+    abstract class Node : NotifyBase {
+        private double x = 200;
+        private double y = 200;
+        public double X { get { return x; } set { x = value; NotifyPropertyChanged(); NotifyPropertyChanged(() => CanvasCenterX); } }
+        public double Y { get { return y; } set { y = value; NotifyPropertyChanged(); NotifyPropertyChanged(() => CanvasCenterY); } }
+
+        public double diameter {get; set; } // Tilføj Notify hvis Diameter skal ændres
+
+        public string visualText;
+
+        public double CanvasCenterX { get { return X + diameter / 2; } set { X = value - diameter / 2; NotifyPropertyChanged(() => X); } }
+
+        public double CanvasCenterY { get { return Y + diameter / 2; } set { Y = value - diameter / 2; NotifyPropertyChanged(() => Y); } }
+    }
+}
---
+++ AlgoTreeDraw/AlgoTreeDraw/ViewModel/NodeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using AlgoTreeDraw.Model;
+using System;
+using GalaSoft.MvvmLight;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace AlgoTreeDraw.ViewModel
+{
+    class NodeViewModel : ViewModelBase
+    {
+
+        public ObservableCollection<BST> BSTNodes {get; set; }
+        public NodeViewModel()
+        {
+            BSTNodes = new ObservableCollection<BST>() {
+                new BST() { X = 30, Y = 40, diameter = 50},
+                new BST() { X = 300, Y = 100, diameter = 50}
+            };
+        }
+    }
+}
