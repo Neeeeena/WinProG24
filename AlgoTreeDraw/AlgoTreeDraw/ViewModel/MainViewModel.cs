50114fd1a3aa4c17763674ab2f018ee22b979357
Alemil <s134073@student.dtu.dk>
2015-10-27 15:49:49 +0100

    Ikke den her

--- AlgoTreeDraw/AlgoTreeDraw/ViewModel/MainViewModel.cs
+++ AlgoTreeDraw/AlgoTreeDraw/ViewModel/MainViewModel.cs
@@ -1,10 +1,4 @@
-using AlgoTreeDraw.Model;
 using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.CommandWpf;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
 
 namespace AlgoTreeDraw.ViewModel
 {
@@ -22,20 +16,11 @@ namespace AlgoTreeDraw.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
-        private Point initialMousePosition;
-        private Point initialShapePosition;
-
-        public ICommand MouseDownNodeCommand { get; }
-        public ICommand MouseMoveNodeCommand { get; }
-
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
         public MainViewModel()
         {
-            System.Console.WriteLine("LOL");
-            MouseDownNodeCommand = new RelayCommand<MouseButtonEventArgs>(MouseDownNode);
-            MouseMoveNodeCommand = new RelayCommand<MouseEventArgs>(MouseMoveNode);
             ////if (IsInDesignMode)
             ////{
             ////    // Code runs in Blend --> create design time data.
@@ -44,75 +29,6 @@ namespace AlgoTreeDraw.ViewModel
             ////{
             ////    // Code runs "for real"
             ////}
-        }
-
-        private void MouseDownNode(MouseButtonEventArgs e)
-        {
-            // Checks that a line is not being drawn.
-
-            //if (!isAddingLine)
-            //{
-            // The Shape is gotten from the mouse event.
-            System.Console.WriteLine("LOL");
-            var shape = TargetShape(e);
-                // The mouse position relative to the target of the mouse event.
-                var mousePosition = RelativeMousePosition(e);
-
-                // When the shape is moved with the mouse, the MouseMoveShape method is called many times, 
-                //  for each part of the movement.
-                // Therefore to only have 1 Undo/Redo command saved for the whole movement, the initial position is saved, 
-                //  during the start of the movement, so that it together with the final position, 
-                //  from when the mouse is released, can become one Undo/Redo command.
-                // The initial shape position is saved to calculate the offset that the shape should be moved.
-                initialMousePosition = mousePosition;
-                initialShapePosition = new Point(shape.X, shape.Y);
-
-                // The mouse is captured, so the current shape will always be the target of the mouse events, 
-                //  even if the mouse is outside the application window.
-                e.MouseDevice.Target.CaptureMouse();
-            //}
-        }
-
-        private void MouseMoveNode(MouseEventArgs e)
-        {
-            
-            // Checks that the mouse is captured and that a line is not being drawn.
-            if (Mouse.Captured != null) //&& !isAddingLine)
-            {
-                // The Shape is gotten from the mouse event.
-                var node = TargetShape(e);
-                // The mouse position relative to the target of the mouse event.
-                var mousePosition = RelativeMousePosition(e);
-
-                // The Shape is moved by the offset between the original and current mouse position.
-                // The View (GUI) is then notified by the Shape, that its properties have changed.
-                node.X = initialShapePosition.X + (mousePosition.X - initialMousePosition.X);
-                node.Y = initialShapePosition.Y + (mousePosition.Y - initialMousePosition.Y);
-            }
-        }
-
-        private Node TargetShape(MouseEventArgs e)
-        {
-            // Here the visual element that the mouse is captured by is retrieved.
-            var nodeVisualElement = (FrameworkElement)e.MouseDevice.Target;
-            // From the shapes visual element, the Shape object which is the DataContext is retrieved.
-            return (Node)nodeVisualElement.DataContext;
-        }
-        
-        private Point RelativeMousePosition(MouseEventArgs e)
-        {
-            // Here the visual element that the mouse is captured by is retrieved.
-            var shapeVisualElement = (FrameworkElement)e.MouseDevice.Target;
-            // The canvas holding the shapes visual element, is found by searching up the tree of visual elements.
-            var canvas = FindParentOfType<Canvas>(shapeVisualElement);
-            // The mouse position relative to the canvas is gotten here.
-            return Mouse.GetPosition(canvas);
-        }
-
-        private static T FindParentOfType<T>(DependencyObject o)
-        {
-            dynamic parent = VisualTreeHelper.GetParent(o);
-            return parent.GetType().IsAssignableFrom(typeof(T)) ? parent : FindParentOfType<T>(parent);
         }
     }
 }
