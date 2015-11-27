b2e026244e5f82f6311f951807c2f5b393b32e59
ccbn <carl.b.nielsen@gmail.com>
2015-11-27 12:47:04 +0100

    Nodes cannot go outside canvas.

--- AlgoTreeDraw/AlgoTreeDraw/ViewModel/MainViewModelBase.cs
+++ AlgoTreeDraw/AlgoTreeDraw/ViewModel/MainViewModelBase.cs
@@ -222,27 +222,43 @@ namespace AlgoTreeDraw.ViewModel
             }
             else
             {
-
-                var mousePosition = RelativeMousePosition(e);
-
-                foreach (NodeViewModel n in selectedNodes)
+                bool isOutside = false;
+                foreach (NodeViewModel n1 in selectedNodes)
                 {
-                    n.X = n.initialNodePosition.X;
-                    n.Y = n.initialNodePosition.Y;
+                    if (n1.X < 0 || n1.Y < 0)
+                    {
+                        isOutside = true;
+                        break;
+                    }
                 }
-
-                if (!(initialMousePosition.X == mousePosition.X && initialMousePosition.Y == mousePosition.Y)) //Only when it actually moves
-            {
-                undoRedo.InsertInUndoRedo(new MoveNodeCommand(selectedNodes, mousePosition.X - initialMousePosition.X, mousePosition.Y - initialMousePosition.Y));
-                foreach (var n in selectedNodes)
+                if (isOutside)
                 {
-                    if (n.X + n.Diameter > CanvasWidth)
-                        CanvasWidth = (int)(n.X + n.Diameter);
-                    if (n.Y + n.Diameter > CanvasHeight)
-                        CanvasHeight = (int)(n.Y + n.Diameter);
-                }
+                    foreach (NodeViewModel n in selectedNodes)
+                    {
+                        n.X = n.initialNodePosition.X;
+                        n.Y = n.initialNodePosition.Y;
+                    }
+                } else { 
+                        var mousePosition = RelativeMousePosition(e);
 
-            }
+                        foreach (NodeViewModel n in selectedNodes)
+                        {
+                            n.X = n.initialNodePosition.X;
+                            n.Y = n.initialNodePosition.Y;
+                        }
+
+                        if (!(initialMousePosition.X == mousePosition.X && initialMousePosition.Y == mousePosition.Y)) //Only when it actually moves
+                    {
+                        undoRedo.InsertInUndoRedo(new MoveNodeCommand(selectedNodes, mousePosition.X - initialMousePosition.X, mousePosition.Y - initialMousePosition.Y));
+                        foreach (var n in selectedNodes)
+                        {
+                            if (n.X + n.Diameter > CanvasWidth)
+                                CanvasWidth = (int)(n.X + n.Diameter);
+                            if (n.Y + n.Diameter > CanvasHeight)
+                                CanvasHeight = (int)(n.Y + n.Diameter);
+                        }
+                        }
+                }
             }
                 e.MouseDevice.Target.ReleaseMouseCapture();
             
@@ -269,15 +285,15 @@ namespace AlgoTreeDraw.ViewModel
                 n.initialNodePosition.Y = n.Y;
             }
 
+            
+            
             e.MouseDevice.Target.CaptureMouse();
             if (e.ClickCount == 2 && e.LeftButton == MouseButtonState.Pressed)
             {
                 node.IsEditing = Visibility.Visible;
                 node.IsNotEditing = Visibility.Hidden;
-                editNode = node;
-                
+                editNode = node;   
             }
-
         }
 
         private void MouseMoveNode(MouseEventArgs e)
@@ -295,11 +311,8 @@ namespace AlgoTreeDraw.ViewModel
                 {
                     tempX = n.initialNodePosition.X + (mousePosition.X - initialMousePosition.X);
                     tempY = n.initialNodePosition.Y + (mousePosition.Y - initialMousePosition.Y);
-                    if (!(tempX < 0 || tempY < 0))
-                    {
-                        n.X = tempX;
-                        n.Y = tempY;
-                    }
+                    n.X = tempX;
+                    n.Y = tempY;
                 }
             }
         }
